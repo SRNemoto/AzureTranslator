47199851dab76dfc3824036658b02ac05202cd63
Shota Nemoto <srn24@case.edu>
2019-11-02 18:04:33 -0400

    Added TransTxt function to handler

--- SpchTxtHandler.py
+++ SpchTxtHandler.py
@@ -1,5 +1,6 @@
 import azure.cognitiveservices.speech as SpeechSDK
 import azure.cognitiveservices.speech.translation as TransSDK
+import os, requests, uuid, json
 
 # filename = "api_key.txt"
 # api_file = open(filename, "r")
@@ -45,3 +46,23 @@ class SpchTxtHandler:
         speech_synthesizer = SpeechSDK.SpeechSynthesizer(speech_config=speech_config)
 
         speech_synthesizer.speak_text_async(input_txt).get()
+
+    def TransTxt(self, input_txt, target_lang):
+        base_url = 'https://api.cognitive.microsofttranslator.com'
+        path = '/translate?api-version=3.0'
+        params = '&to=' + target_lang
+        constructed_url = base_url + path + params
+
+        headers = {
+            'Ocp-Apim-Subscription-Key': self._key,
+            'Ocp-Apim-Subscription-Region': self._region,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': str(uuid.uuid4())
+        }
+
+        # You can pass more than one object in body.
+        body = [{
+            'text' : input_txt
+        }]
+        response = requests.post(constructed_url, headers=headers, json=body)
+        return response.json()[0]['translations'][0]['text']
